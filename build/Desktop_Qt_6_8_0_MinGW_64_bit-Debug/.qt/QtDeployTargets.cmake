cfa3aefbc22548ec86cbb4c97e0bc7c23ec005c7
hillo <hillo3127@gmail.com>
2024-11-14 13:33:36 +1300

    Initialise Project

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_StudyApp_FILE C:/Users/Hillo/Downloads/Programing/Study-App/StudyApp/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/StudyApp.exe)
+set(__QT_DEPLOY_TARGET_StudyApp_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_StudyApp_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
